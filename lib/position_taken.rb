ed12fcdc6e0468e1378d5d718473e6b543a770bc
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-03 17:57:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,7 @@ def position_taken?(board, index)
       return false
   end
 
-  if board = "X" || board = "O" && index = "X" || index = "O"
+  if board && index = "X" || board && index = "O"
     return true
   end
 
