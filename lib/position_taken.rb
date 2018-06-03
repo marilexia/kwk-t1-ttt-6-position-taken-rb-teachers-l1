a8302813393e65148853d8940dc1249cea1babda
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-03 18:11:18 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -9,12 +9,12 @@ def position_taken?(board, index)
       return false
   end
 
-  if index = "X" || index = "O"
+  # if index = "X" || index = "O"
+  #   return true
+  # end
+
+  if board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
     return true
   end
-  
-  # if
-  #
-  # end
 
 end
