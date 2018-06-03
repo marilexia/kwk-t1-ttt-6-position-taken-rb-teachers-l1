a101aa43b4e9456eacaaf3dec18a2480ce72d8e1
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-03 18:44:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,25 @@
 # code your #position_taken? method here!
+
+
+
+# def position_taken?(board, index)
+#
+#  if board = " " || board = "" || nil
+#
+#       return false
+#   end
+#
+# if !(board = " " || board = "" || nil)
+#     return true
+#   end
+#
+# end
+
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+     true
+  end
+end
